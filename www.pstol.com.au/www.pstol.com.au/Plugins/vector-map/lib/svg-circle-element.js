812ab7b88da5fa423d84db87eb7e898a5b253c9d
john.hadikusumo@gmail.com <Gunawan70>
2015-02-22 22:03:57 +1100

    update
    update

---
+++ www.pstol.com.au/www.pstol.com.au/Plugins/vector-map/lib/svg-circle-element.js
@@ -0,0 +1,5 @@
+﻿jvm.SVGCircleElement = function (config, style) {
+    jvm.SVGCircleElement.parentClass.call(this, 'circle', config, style);
+};
+
+jvm.inherits(jvm.SVGCircleElement, jvm.SVGShapeElement);
